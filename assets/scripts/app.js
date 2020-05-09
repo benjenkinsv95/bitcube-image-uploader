69763fb55e0cba246454ae3934396107e2ddd066
benjenkinsv95 <benjenkinsv95@gmail.com>
2020-05-09 07:58:00 -0400

    Add images index

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,6 +8,30 @@
 
 
 $(() => {
+  const indexUploads = function () {
+    return $.ajax({
+      url: "http://localhost:4741/uploads"
+    });
+  }
+
+  const indexUploadsSuccessful = responseData => {
+    let allUploadsHtml = ``;
+    responseData.uploads.forEach(upload => {
+      const uploadHtml = `
+        <div class='uploaded-image' style='background-image: url(${upload.path})'>
+          <h2>${upload.title}</h2>
+        </div>
+        `;
+      allUploadsHtml += uploadHtml;
+    });
+    $('.all-images-container').html(allUploadsHtml);
+  };
+
+  // Show the grid of all uploads on page load
+  indexUploads()
+    .then(indexUploadsSuccessful)
+    .catch(console.error)
+
   $("#upload-image").on("submit", (event) => {
     event.preventDefault()
     
@@ -21,8 +45,20 @@ $(() => {
       contentType: false,
       data: formData
     })
-      .then(() => console.log("success"))
+      .then(responseData => {
+        $('.image-container').css('background-image', `url(${responseData.upload.path})`)
+        return indexUploads()
+      })
+      .then(indexUploadsSuccessful)
       .catch(console.error);
-  });
+  })
+
+  
+
+  
+
+ 
 });
+
+
 

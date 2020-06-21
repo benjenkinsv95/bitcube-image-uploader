1ad941aad5fb076101e5ed4d623b1bf048fed9d1
benjenkinsv95 <benjenkinsv95@gmail.com>
2020-06-21 11:16:16 -0400

    Add image popups that allow you to copy to clipboard

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,40 +7,44 @@
 // require('./example')
 
 const { apiUrl } = require('./config')
+const copy = require('copy-to-clipboard')
+
 $(() => {
+  let uploads = []
   const indexUploads = function () {
     return $.ajax({
       url: `${apiUrl}/uploads`
-    });
+    })
   }
 
   const indexUploadsSuccessful = responseData => {
-    let allUploadsHtml = ``;
-    responseData.uploads.forEach(upload => {
+    uploads = responseData.uploads
+    let allUploadsHtml = ``
+    uploads.forEach(upload => {
       const uploadHtml = `
-        <div class='uploaded-image' style='background-image: url(${upload.path})'>
+        <div class='uploaded-image' style='background-image: url(${upload.path})' data-image-url="${upload.path}" data-image-title="${upload.title}">
           <h2>${upload.title}</h2>
         </div>
-        `;
-      allUploadsHtml += uploadHtml;
-    });
-    $('.all-images-container').html(allUploadsHtml);
-  };
+        `
+      allUploadsHtml += uploadHtml
+    })
+    $('.all-images-container').html(allUploadsHtml)
+  }
 
   // Show the grid of all uploads on page load
   indexUploads()
     .then(indexUploadsSuccessful)
     .catch(console.error)
 
-  $("#upload-image").on("submit", (event) => {
+  $('#upload-image').on('submit', (event) => {
     event.preventDefault()
-    
-    const form = event.target;
-    const formData = new FormData(form);
+
+    const form = event.target
+    const formData = new FormData(form)
 
     $.ajax({
       url: `${apiUrl}/uploads`,
-      method: "POST",
+      method: 'POST',
       processData: false,
       contentType: false,
       data: formData
@@ -50,15 +54,46 @@ $(() => {
         return indexUploads()
       })
       .then(indexUploadsSuccessful)
-      .catch(console.error);
+      .catch(console.error)
   })
 
-  
+  $('#file-upload').on('change', () => {
+    const form = $('#upload-image')[0]
+    const formData = new FormData(form)
 
-  
+    if (formData.get('title').trim() === '') {
+      const image = formData.get('image')
+      formData.set('title', image.name)
+    }
 
- 
-});
+    $.ajax({
+      url: `${apiUrl}/uploads`,
+      method: 'POST',
+      processData: false,
+      contentType: false,
+      data: formData
+    })
+      .then(responseData => {
+        $('.image-container').css('background-image', `url(${responseData.upload.path})`)
+        return indexUploads()
+      })
+      .then(indexUploadsSuccessful)
+      .catch(console.error)
+  })
 
+  $('.all-images-container').on('click', '.uploaded-image', event => {
+    const imageUrl = $(event.target).data('image-url')
+    const imageTitle = $(event.target).data('image-title')
 
+    $("input[name='image-url']").val(imageUrl)
+    $('#selected-img').attr('src', imageUrl)
+    $('#selected-img-title').text(imageTitle)
 
+    $('#image-modal').modal('show')
+  })
+
+  $('#copy-to-clipboard').on('click', event => {
+    const imageUrl =  $("input[name='image-url']").val()
+    copy(imageUrl)
+  })
+})
